c5eca25cb6902cb1f2b9a5eeddbde5c6cb22ba96
manummimavindya <manummi2023@gmail.com>
2025-09-22 10:26:39 +0530

    init commit

---
+++ src/main/java/lk/ijse/elitedrivingschoolproject/bo/BOTypes.java
@@ -0,0 +1,12 @@
+package lk.ijse.elitedrivingschoolproject.bo;
+
+public enum BOTypes {
+    COURSE,
+    INSTRUCTOR,
+    LESSONS,
+    PAYMENTS,
+    QUERY,
+    STUDENT_COURSE_DETAILS,
+    STUDENT,
+    USER
+}
