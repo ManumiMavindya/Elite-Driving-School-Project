bca61bf5c78d86184d953afc7b772257a72b07cf
manummimavindya <manummi2023@gmail.com>
2025-09-22 10:32:18 +0530

    create crudDAO

--- src/main/java/lk/ijse/elitedrivingschoolproject/dao/CrudDAO.java
+++ src/main/java/lk/ijse/elitedrivingschoolproject/dao/CrudDAO.java
@@ -1,4 +1,23 @@
 package lk.ijse.elitedrivingschoolproject.dao;
 
-public interface CrudDAO {
+import java.util.List;
+import java.util.Optional;
+
+public interface CrudDAO<T> extends SuperDAO {
+
+    List<T> getAll() throws Exception;
+
+    String getLastId() throws Exception;
+
+    boolean save(T entity) throws Exception;
+
+    boolean update(T entity) throws Exception;
+
+    boolean delete(String id) throws Exception;
+
+    List<String> getAllIds() throws Exception;
+
+    Optional<T> findById(String id) throws Exception;
+
+    String generateNewId() throws Exception;
 }
