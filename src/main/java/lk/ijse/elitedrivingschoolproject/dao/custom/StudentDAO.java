849d407ca412a1e140206a5dabddcf1f28e0b78d
manummimavindya <manummi2023@gmail.com>
2025-09-24 14:06:02 +0530

    create StudentsDAO

--- src/main/java/lk/ijse/elitedrivingschoolproject/dao/custom/StudentDAO.java
+++ src/main/java/lk/ijse/elitedrivingschoolproject/dao/custom/StudentDAO.java
@@ -1,4 +1,7 @@
 package lk.ijse.elitedrivingschoolproject.dao.custom;
 
-public interface StudentDAO {
+import lk.ijse.elitedrivingschoolproject.dao.CrudDAO;
+import lk.ijse.elitedrivingschoolproject.entity.Students;
+
+public interface StudentDAO extends CrudDAO<Students> {
 }
