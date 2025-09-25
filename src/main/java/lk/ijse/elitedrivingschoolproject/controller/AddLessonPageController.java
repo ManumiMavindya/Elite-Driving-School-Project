1055ce1b0169d5b6be7a57f3f4b7471190e9d935
manummimavindya <manummi2023@gmail.com>
2025-09-25 17:21:50 +0530

    init commit

--- src/main/java/lk/ijse/elitedrivingschoolproject/controller/AddLessonPageController.java
+++ src/main/java/lk/ijse/elitedrivingschoolproject/controller/AddLessonPageController.java
@@ -1,4 +1,44 @@
 package lk.ijse.elitedrivingschoolproject.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+
 public class AddLessonPageController {
+
+    @FXML
+    private Button addLessonbtn;
+
+    @FXML
+    private ComboBox<?> coursecmb;
+
+    @FXML
+    private ComboBox<?> endcmb;
+
+    @FXML
+    private ComboBox<?> instructorcmb;
+
+    @FXML
+    private DatePicker lessonDatepicker;
+
+    @FXML
+    private Label lessonIdlbl;
+
+    @FXML
+    private ComboBox<?> startcmb;
+
+    @FXML
+    private ComboBox<?> statuscmb;
+
+    @FXML
+    private ComboBox<?> studentcmb;
+
+    @FXML
+    void addLessonbtnOnAction(ActionEvent event) {
+
+    }
+
 }
