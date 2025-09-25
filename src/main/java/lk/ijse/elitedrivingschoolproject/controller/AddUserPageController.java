2f3f97ef488ddec52e7b521decd9897606d39ea3
manummimavindya <manummi2023@gmail.com>
2025-09-25 17:44:30 +0530

    init commit

---
+++ src/main/java/lk/ijse/elitedrivingschoolproject/controller/AddUserPageController.java
@@ -0,0 +1,38 @@
+package lk.ijse.elitedrivingschoolproject.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class AddUserPageController {
+
+    @FXML
+    private Button addUserbtn;
+
+    @FXML
+    private TextField userAgetxt;
+
+    @FXML
+    private TextField userContacttxt;
+
+    @FXML
+    private TextField userEmailtxt;
+
+    @FXML
+    private Label userIdlbl;
+
+    @FXML
+    private TextField userNametxt;
+
+    @FXML
+    private TextField userPasswordtxt;
+
+    @FXML
+    void addUserbtnOnAction(ActionEvent event) {
+
+    }
+
+}
+
--- src/main/java/lk/ijse/elitedrivingschoolproject/controller/CoursesPageController.java
+++ src/main/java/lk/ijse/elitedrivingschoolproject/controller/CoursesPageController.java
@@ -1,4 +1,44 @@
 package lk.ijse.elitedrivingschoolproject.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class CoursesPageController {
+
+    @FXML
+    private Button addCoursebtn;
+
+    @FXML
+    private TableColumn<?, ?> colAction;
+
+    @FXML
+    private TableColumn<?, ?> colCourseId;
+
+    @FXML
+    private TableColumn<?, ?> colCourseName;
+
+    @FXML
+    private TableColumn<?, ?> colDescription;
+
+    @FXML
+    private TableColumn<?, ?> colDuration;
+
+    @FXML
+    private TableColumn<?, ?> colFee;
+
+    @FXML
+    private TableView<?> coursetbl;
+
+    @FXML
+    private TextField searchtxt;
+
+    @FXML
+    void addCoursebtn(ActionEvent event) {
+
+    }
+
 }
